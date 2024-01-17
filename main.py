738f852d45d3a98409fd24a4bae35e2245f6bce2
Tiego <96924579+tiegotc@users.noreply.github.com>
2024-01-17 23:50:56 +0200

    Update main.py
    add arguments functionality

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import utils
 from models import LLMS
 import yaml
+import argparse
 import warnings
 warnings.filterwarnings('ignore')
+parser = argparse.ArgumentParser()
 
 with open("./config.yml", 'r') as file:
     inputs = yaml.safe_load(file)
 
-def run():
-    print(inputs["review_list"][0])
-    model = LLMS(input_sentence=inputs["input_sentence"], review_list=inputs["review_list"])
-    model.sentiment_analyser()
-    model.zeroshot_classifier()
+parser.add_argument('--input_text', 
+                    type=str, 
+                    default=inputs['input_sentence'],
+                    help='Input text to pass into the sentiment analyser.')
+parser.add_argument('--review_list', 
+                    type=list, 
+                    default=inputs['review_list'],
+                    help='A list of reviews that you want to check the sentiment for iteratively.')
+
+def run(args):
+        model = LLMS(input_sentence=args.input_text, review_list=args.review_list)
+        model.sentiment_analyser()
+        model.zeroshot_classifier()
 
 if __name__ == "__main__":
-    run()
+    args = parser.parse_args()
+    run(args)
